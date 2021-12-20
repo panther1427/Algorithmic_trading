051956521bf39a045cff5da3dfee33f32da464e8
panther1427 <91949881+panther1427@users.noreply.github.com>
2021-12-20 22:09:17 +0100

    Update Kalman_filter_framework.py

--- Kalman_filter_framework.py
+++ Kalman_filter_framework.py
@@ -9,6 +9,7 @@ from collections import deque
 
 class CointegrationAndKalmanFilter(QCAlgorithm):
     def Initialize(self):
+        #setting our start, end etc
         self.SetStartDate(2016, 4, 28)  # Set Start Date
         self.SetEndDate(2021, 5, 30)  # Set End Date
         self.SetCash(100000)  # Set Strategy Cash
@@ -17,19 +18,24 @@ class CointegrationAndKalmanFilter(QCAlgorithm):
         self.SetBrokerageModel(BrokerageName.InteractiveBrokersBrokerage, AccountType.Margin)
         self.SetWarmup(timedelta(days = 7))
         
+        #setting our universes and alphas etc
         self.AddUniverse(self.CoarseUniverse, self.FineUniverse)
         self.SetPortfolioConstruction(EqualWeightingPortfolioConstructionModel(rebalance = timedelta(weeks=1), portfolioBias = PortfolioBias.LongShort))
         self.SetExecution(ImmediateExecutionModel())
         self.AddAlpha(PairsTradingAlpha())
         
-        self.num_coarse = 40
         #has to be a even number, or it wont be market neutral, or work at all
+        self.num_coarse = 40
+    
+        #Used for rebalancing
         self.lastMonth = -1
         
+        #our resolution and lookback for calculating the cointegration
         self.resolution = Resolution.Daily
         self.lookback = timedelta(weeks=150)
 
     def OnEndOfDay(self):
+        #Used for plotting different things
         self.Plot("Positions", "Num", len([x.Symbol for x in self.Portfolio.Values if self.Portfolio[x.Symbol].Invested]))
         self.Plot(f"Margin", "Used", self.Portfolio.TotalMarginUsed)
         self.Plot(f"Margin", "Remaining", self.Portfolio.MarginRemaining)
@@ -37,35 +43,40 @@ class CointegrationAndKalmanFilter(QCAlgorithm):
 
         
     def CoarseUniverse(self, coarse):
+        #Rebalance function, only rebalances our univers once pr month
         if self.Time.month == self.lastMonth:
             return Universe.Unchanged
         self.lastMonth = self.Time.month
 
+        #selects stocks based on the stocks having fundamental data, and price over 15 dollars. Sorts it by volume
         selected = sorted([x for x in coarse if x.HasFundamentalData and x.Price > 15], 
                         key = lambda x: x.DollarVolume, reverse = True)
 
         return [x.Symbol for x in selected[:self.num_coarse]]
     
     def FineUniverse(self, fine):
+        #returns only the tickers
         filtered_fine = [x.Symbol for x in fine]
         
+        #Used to get our price history
         history = self.make_and_unstack_dataframe(filtered_fine)
-
-        global pvalue_matrix
+        
+        #retuns our matrix and the pairs
         pvalue_matrix, pairs = self.find_cointegrated_pairs(history)
         
-        #Aktier bliver appended hvis de er i vores filtered fine
+        #appends the pairs to a list, so we get one long list
         stocks = []
         for pair in pairs:
             stocks.append(pair[0])
             stocks.append(pair[1])
  
-        #vælg de aktier som har en positiv zscore, aktierne ligger i pairs af hveranden
+        #makes a list and returns it, if the stock is in the filtered_fine list
         final_stocks = [x for x in filtered_fine if str(x) in stocks]
         return final_stocks
 
 
     def make_and_unstack_dataframe(self, list1):
+        #makes and unstacks the dataframe
         dataframe = self.History(list1, self.lookback, self.resolution)
         dataframe = dataframe['close'].unstack(level=0)
         dataframe = dataframe.dropna(axis=1)
@@ -83,6 +94,7 @@ class CointegrationAndKalmanFilter(QCAlgorithm):
             for ii in range(i+1, n): 
                 stock1 = dataframe[keys[i]]
                 stock2 = dataframe[keys[ii]]
+                #The cointegration part, that calculates cointegration between 2 stocks
                 result = sm.tsa.stattools.coint(stock1, stock2) 
                 pvalue = result[1] 
                 pvalue_matrix[i, ii] = pvalue
@@ -95,40 +107,46 @@ class CointegrationAndKalmanFilter(QCAlgorithm):
 
 class PairsTradingAlpha(AlphaModel):
     def __init__(self, resolution = Resolution.Daily, lookback = timedelta(weeks = 10), predictionInterval = timedelta(weeks=1)):
-        #Vi sætter en lavere resolution, idet at vi ikke skal cointegrere, men beregne spread osv, hvor vi ikke har brug for så meget data
+        #setting our resolution lookback etc
         self.resolution = resolution
         self.lookback = lookback
         self.predictionInterval = predictionInterval
         
-        #Holder øje med de par vi har i vores algo
+        #Keeps track of the pairs
         self.pairs = dict()
-        #Holder øje med alle symboler vi har i vores liste
+        #keeps track of all the stock, not listed by pairs
         self.Securities = list()
         
     def Update(self, algorithm, data):
         
+        #our insight list, which we will return when the looping  is done
         insights =[]  
 
+        #loops through our dictionary
         for key, symbolData in self.pairs.items():
             
+            #uses the function, and gives 3 paramteres
             df, stock_y, stock_x = self.PairsToListAndHistory(algorithm, symbolData.pair_symbol)
             
+            #calculates the kalman filter, and calculates the spread
             spread, lower, middle, upper = self.setKalman(df, stock_y, stock_x)
             
+            #Sets the prev state, to the state before the trading logic takes place
             previous_state = symbolData.State
             
             
-            #implement the trading logic. Also check if the stock is already invested
-            
+            #sets the direction to flat
             stock_y_direction = InsightDirection.Flat
             stock_x_direction = InsightDirection.Flat
             
             #Hvis at spread er over std, så skal y stige, og x skal falde. Hvis spread er under std, skal y falde, og x stige
             
-            ###Implementer State, og det skal gøres så når values kommer ind for første gang, får den en flat state
-
+            
+            #Our logic to make trades. If the pairs is already invested, we go into the loop
             if symbolData.IfInvested == 1:
+                #If we have gone short on the spread, enter this logic
                 if previous_state == 1:
+                    #If the spread returns to mean, we liquidate
                     if spread <= middle:
                         stock_x_direction = InsightDirection.Flat
                         stock_y_direction = InsightDirection.Flat
@@ -136,50 +154,58 @@ class PairsTradingAlpha(AlphaModel):
                     elif spread >= middle:
                         continue
                     
+                #If we have gone short on the spread, enter this logic
                 if previous_state == -1:
+                    #If spread has returned to the mean
                     if spread >= middle:
                         stock_x_direction = InsightDirection.Flat
                         stock_y_direction = InsightDirection.Flat
+                    #if spread dont go over middle, we do nothing
                     elif spread <= middle:
                         continue
             
-            
+            #If we have not invested in the spread, enter this logic
             if symbolData.IfInvested == 0:
+                #If we have done nothing about the spread
                 if previous_state == 0:
+                    #If spread is over 2 std
                     if spread >= upper:
                         stock_y_direction = InsightDirection.Up
                         stock_x_direction = InsightDirection.Down
-                    #sæt invested til 1, da vi har købt
+                    #If spread is under 2 std
                     elif spread <= lower:
                         stock_y_direction = InsightDirection.Down
                         stock_x_direction = InsightDirection.Up
-                    #sæt invested til 1, da vi har købt
+                  #Else we do nothing
                 else:
                     continue
                 
                 
-                    
+            #If we have liqudiated or done nothing, set ifinvested and state to 0   
             if stock_x_direction and stock_y_direction == 0:
                 symbolData.IfInvested = 0
                 symbolData.State = 0
-                
+            
+            #If we have gone short the spread, set the following parameteres    
             elif stock_x_direction == -1 and stock_y_direction == 1:
                 symbolData.IfInvested = 1
                 symbolData.State = -1
-                
+            
+            #If we have gone long the spread, set the following parameteres    
             elif stock_x_direction == 1 and stock_y_direction == -1:
                 symbolData.IfInvested = 1
                 symbolData.State = 1
                 
               
-                    
+            #Set our insights to the right ticker       
             insight_y = Insight(stock_y, self.predictionInterval, InsightType.Price, stock_y_direction)
             insight_x = Insight(stock_x, self.predictionInterval, InsightType.Price, stock_x_direction)
             
+            #this is not necessary
             updated_dict = (stock_x, stock_y)
             symbolData.pair_symbol = updated_dict
             
-            #brug extend og ikke append når vi skal tilføje en gruppe af insights
+            #If we have changed state (bought, or liqudiated) we extend the insight list
             if symbolData.State != previous_state:
                 insights.extend(Insight.Group(insight_y, insight_x))
                 algorithm.Log(f"Pairs of traded stocks is {stock_y} and {stock_x}, and the direction is {symbolData.State}")
@@ -202,7 +228,7 @@ class PairsTradingAlpha(AlphaModel):
         for security in changes.RemovedSecurities:
             del_keys = []
             
-            #måske brug den her logik? ### [k for k in self.pairs.keys() if security.Symbol in k] ###
+            #For removing securities in the self.pairs
             for key, symbolData, in self.pairs.items():
                 if security in symbolData.pair_symbol:
                     del_keys.append(key)
@@ -210,14 +236,17 @@ class PairsTradingAlpha(AlphaModel):
             for key in del_keys:
                 self.pairs.pop(key)
                 
-                
+        #update the pairs        
         self.UpdatePairs(algorithm)
         
         
     def UpdatePairs(self, algorithm):
-        #bliver brugt til at updatere vores pairs, og at smide dem ind i listen
+        #use the list of the active tickers in our univers
         symbols = [x.Symbol for x in self.Securities]
         
+        #
+        #
+        #This is maybe wrong logic, have to double check!!!!!
         for i in range(0, len(symbols)):
             asset_i = symbols[i]
             
@@ -233,51 +262,62 @@ class PairsTradingAlpha(AlphaModel):
                     continue
                 
                 self.pairs[i] = symbolData(pair_symbol)
-
+        #
+        #
+        #
+        
     def PairsToListAndHistory(self, algorithm, pair):
-        #Virker som den skal. Funktion til at få historie
+        #set the stocks in the list
         stocks = list(pair)
         stock1 = stocks[0]
         stock2 = stocks[1]
-        #dette parameter bruger vi, så vi kan finde ud af om vi allerede har investeret i disse to par. Vi sætter det til 0, da vi ikke har investeret endnu
+        #get the price data for the stocks
         df1 = algorithm.History(stock1, self.lookback, self.resolution)
         df2 = algorithm.History(stock2, self.lookback, self.resolution)
-        #smart løsning hvis 2 df skal sættes sammen korrekt i QC
+        #good solution the melt the dataframes together in a thight and fast way. Uses pandas
         history = pd.concat([df1, df2], axis=0)
         history = history['close'].unstack(level=0)
         history = history.dropna(axis=1)
+        #Return the stock stocks, and the history of the 2 stocks
         return history, stock1, stock2
 
 
     
     def setKalman(self, df, stock1, stock2):
+        #set the data for the stocks as y and x
         x = df[stock1]
         y = df[stock2]
     
+        #Make a dataframe, that melt them together
         df1 = pd.DataFrame({'y':y, 'x':x})
+        #Set index
         df1.index = pd.to_datetime(df1.index)
     
-
+        #Get the kalman filter and calculate the spread
         state_means = self.regression(self.avg(x), self.avg(y))
         df1['hr'] = - state_means[:, 0]
         df1['spread'] =  df1.y + (df1.x * df1.hr)
     
+        #Set theta and mean
         dt = 1
         mu = np.average(df1.spread)
         theta = 1
-    
+        
+        #Set the standard deviation and variance
         sigma = np.std(df1['spread'])
         ts = np.arange(0, len(df1['spread'].values), dt)
         var = np.array([sigma**2 / (2 * theta) * (1-np.exp(-2 * theta * t)) for t in ts])
         std = 2 * np.sqrt(var)
         std = std[-1]
+        
+        #Calculate the upper and lower threshold
         upper = mu + std
         lower = mu - std
         
-        
+        #Return the last spread data, lower, mean and upper
         return df1.spread[-1], lower, mu, upper
         
-
+    #calculate kalman avg
     def avg(self, x):
         filter = KalmanFilter(transition_matrices = [1],
         observation_matrices = [1],
@@ -288,7 +328,8 @@ class PairsTradingAlpha(AlphaModel):
         spread, _ = filter.filter(x.values)
         spread = pd.Series(spread.flatten(), index = x.index)
         return spread
-
+    
+    #calculate kalman of the 2 stocks
     def regression(self, x ,y):
         x = self.avg(x)
         y = self.avg(y)
@@ -306,12 +347,13 @@ class PairsTradingAlpha(AlphaModel):
 
 class symbolData:
     
+    #Set the state, pairs and ifInvested
     def __init__(self, pair_symbol):
         self.pair_symbol = pair_symbol
         self.State = State.FlatRatio
         self.IfInvested = 0
 
-
+#The state class
 class State(Enum):
     ShortRatio = -1
     FlatRatio = 0
